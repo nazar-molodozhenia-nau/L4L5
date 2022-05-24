2a7c83c2ae9a8879f98668efa29f834946bd09ca
nazar-molodozhenia-nau <6868491@stud.nau.edu.ua>
2022-05-24 10:52:43 +0300

    Update PL UI StorageScene "Working Tables" and "Hierarchy"

--- PL/UI/Scenes/StorageScene.xaml.cs
+++ PL/UI/Scenes/StorageScene.xaml.cs
@@ -71,6 +71,8 @@ namespace UI.Scenes {
 
         public int? SelectedId { get; set; }
         public int? SelectedIdNewTable { get; set; }
+        public int? SaveParentId { get; set; }
+        public int? SaveTapId { get; set; }
 
         private void StorageScene_Loaded(object sender, System.Windows.RoutedEventArgs e) { }
 
@@ -85,12 +87,13 @@ namespace UI.Scenes {
         // Grid
 
         private void UpdateDataGrid() { StorageDataGrid.ItemsSource = _storageController.GetAll(); }
+
         private void UpdateDataGrid_OpenStorage() {
             ArrayList ListAll = new ArrayList();
             List<FolderModel> ListFolder = _folderController.GetAll();
             List<FileModel> ListFile = _fileController.GetAll();
-            foreach(FileModel file in ListFile) { if(file.IdParent == (int)SelectedId) { ListAll.Add(file); } }
-            foreach(FolderModel folder in ListFolder) { if(folder.IdParent == (int)SelectedId) { ListAll.Add(folder); } }
+            foreach(FileModel file in ListFile) { if(file.IdParent == SaveParentId) { ListAll.Add(file); } }
+            foreach(FolderModel folder in ListFolder) { if(folder.IdParent == SaveParentId) { ListAll.Add(folder); } }
             StorageAllDataGrid.ItemsSource = ListAll;
         }
 
@@ -109,11 +112,19 @@ namespace UI.Scenes {
             UpdateDataGrid(); ClearFields();
         }
 
+        List<int> ControllerId;
+        int Depth { get; set; } = 0;
+
         private void OpenStorageButton(object sender, RoutedEventArgs e) {
             if(ComboBoxStorageSpecificType.SelectedItem == null || TextBoxOwner.Text.Trim().Length == 0) { return; }
             if(SelectedId != null) {
                 Storage.Id = (int)SelectedId; _storageController.Update(Storage);
                 mainGrid.Visibility = Visibility.Collapsed; OpenStorageGrid.Visibility = Visibility;
+                // Set SaveParentId and/or SaveTapId && Depth
+                SaveParentId = Storage.Id;
+                Depth = 1;
+                ControllerId = new List<int>();
+                ControllerId.Add((int)SaveParentId);
                 UpdateTextBlock(); UpdateDataGrid_OpenStorage();
             }
         }
@@ -136,9 +147,11 @@ namespace UI.Scenes {
 
         // Folder
 
+        int CounterFolder { get; set; }
+
         private void AddFolderButton(object sender, System.Windows.RoutedEventArgs e) {
             if(TextBoxFolderName.Text.Trim().Length == 0) { return; }
-            Folder.IdParent = Storage.Id;
+            Folder.IdParent = (int)SaveParentId;
             Folder.Name = TextBoxFolderName.Text;
             _folderController.Add(Folder);
             UpdateDataGrid_OpenStorage(); ClearFields_OpenStorage();
@@ -146,20 +159,31 @@ namespace UI.Scenes {
 
         private void OpenFolderButton(object sender, RoutedEventArgs e) {
             if(TextBoxFolderName.Text.Trim().Length == 0) { return; }
-            //WeControlScenes.Content = _openStorageScene;
+            if(SelectedIdNewTable != null) {
+                // Id is Not Set
+                //Folder.Id = (int)SelectedIdNewTable + 1000; _folderController.Update(Folder);
+                // Set SaveParentId and/or SaveTapId && Depth
+                SaveParentId = Folder.Id;
+                Depth++;
+                ControllerId.Add((int)SaveParentId);
+                ButtonBack_MovementInStorage.Visibility = Visibility;
+                UpdateDataGrid_OpenStorage();
+            }
         }
 
         private void DeleteFolderButton(object sender, System.Windows.RoutedEventArgs e) {
             if(TextBoxFolderName.Text.Trim().Length == 0) { return; }
-            if(SelectedId != null) { _folderController.Remove((int)SelectedId); }
+            if(SelectedIdNewTable != null) { _folderController.Remove((int)SelectedIdNewTable); }
             UpdateDataGrid_OpenStorage(); ClearFields_OpenStorage();
         }
 
         // File
 
+        int CounterFile { get; set; } = 2000;
+
         private void AddFileButton(object sender, System.Windows.RoutedEventArgs e) {
             if(TextBoxFileName.Text.Trim().Length == 0) { return; }
-            File.IdParent = Storage.Id;
+            File.IdParent = (int)SaveParentId;
             File.Name = TextBoxFileName.Text;
             _fileController.Add(File);
             UpdateDataGrid_OpenStorage(); ClearFields_OpenStorage();
@@ -167,13 +191,20 @@ namespace UI.Scenes {
 
         private void DeleteFileButton(object sender, System.Windows.RoutedEventArgs e) {
             if(TextBoxFileName.Text.Trim().Length == 0) { return; }
-            if(SelectedId != null) { _fileController.Remove((int)SelectedId); }
+            if(SelectedIdNewTable != null) { _fileController.Remove((int)SelectedIdNewTable); }
             UpdateDataGrid_OpenStorage(); ClearFields_OpenStorage();
         }
 
         // Back in Main
 
-        private void BackButton(object sender, System.Windows.RoutedEventArgs e) { OpenStorageGrid.Visibility = Visibility.Collapsed; mainGrid.Visibility = Visibility; }
+        private void BackButton__MovementInStorage(object sender, System.Windows.RoutedEventArgs e) {
+            SaveParentId = ControllerId[Depth - 2];
+            Depth--;
+            if(Depth == 1) { ButtonBack_MovementInStorage.Visibility = Visibility.Hidden; }
+            UpdateDataGrid_OpenStorage(); ClearFields_OpenStorage();
+        }
+
+        private void BackButton_MainMenu(object sender, System.Windows.RoutedEventArgs e) { Depth = 0; OpenStorageGrid.Visibility = Visibility.Collapsed; mainGrid.Visibility = Visibility; }
 
         // OpenStorage MouseDoubleClick
 
